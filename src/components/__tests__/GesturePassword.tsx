487eb6dc0381bd1535f91cb50cfd7800cc6b3260
LEE's MAC <lcz@mzmedi.com>
2019-02-21 20:17:28 +0800

    테스트 삭제

--- src/components/__tests__/GesturePassword.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import GesturePassword from '../GesturePassword';
-
-import renderer from 'react-test-renderer';
-
-test('renders correctly', () => {
-  const tree = renderer.create(<GesturePassword />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
